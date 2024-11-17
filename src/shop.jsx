4ea6d0fcd55d7e278a80bd6a9531bd5eca2405f2
mastachii <mastachii273@gmail.com>
2024-11-18 01:22:00 +0800

    FEAT: Added Shop Dummy API

--- src/shop.jsx
+++ src/shop.jsx
@@ -1,21 +1,10 @@
 import Item from "./components/item";
 import { Link } from "react-router-dom";
 
-const items = (() => {
-    const arr = [];
-
-    for (let i = 1; i <= 5; i++) {
-        arr.push(<Item value={i} />);
-    }
-
-    return arr;
-})();
-
 function Shop() {
     return (
         <div>
             <p>SHOP</p>
-            {items}
             <Link to="/">GO BACK</Link>
             <hr />
             <Link to="cart">TO CART</Link>
