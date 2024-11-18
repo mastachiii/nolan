7036142d57c076137bf561b327083cb119036e72
mastachiii <mastachii273@gmail.com>
2024-11-18 10:12:00 +0800

    feat: adding cart api functions

--- src/scripts/shop.js
+++ src/scripts/shop.js
@@ -1,5 +1,30 @@
 import axios from "axios";
 
 function getProducts() {
-    return axios.get()
+    const request = axios.get("http://localhost:3001/products");
+
+    return request.then((r) => r.data.items);
 }
+
+function getCart() {
+    const request = axios.get("http://localhost:3001/cart");
+
+    return request.then((r) => r.data.items);
+}
+
+// Get current cart array first then append new item.
+async function addItemsToCart(foo) {
+    let cart;
+
+    await getCart().then((r) => (cart = r));
+
+    cart.push(foo);
+
+    const request = axios.put("http://localhost:3001/cart", cart);
+
+    return request.then((r) => console.log(r.data));
+}
+
+addItemsToCart("123");
+
+export { getProducts };
