9ada359cfb2e18a5818333ccc5c2f31aa11b473c
mastachii <mastachii273@gmail.com>
2024-11-20 12:54:15 +0800

    FEAT: Added Image Links for Carousel Component

--- src/components/imageCarousel.jsx
+++ src/components/imageCarousel.jsx
@@ -2,7 +2,26 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 
 function Carousel() {
-    const images = [1, 2, 3, 4, 5, 6];
+    const images = [
+        {
+            1: {
+                title: "The Godfather",
+                image: "https://image.tmdb.org/t/p/original/vJ8H8skNbNzXWVyDNSgxTZMTPA6.jpg",
+            },
+            2: {
+                title: "My Neighbor Totoro",
+                image: "https://image.tmdb.org/t/p/original/6O1mOoTXuc1WqjKd2R7MFQHZ7Eb.jpg",
+            },
+            3: {
+                title: "The Pianist",
+                image: "https://image.tmdb.org/t/p/original/cqOmm81h5zVl182kzbr0xSKD6nF.jpg",
+            },
+            4: {
+                title: "Past Lives",
+                image: "https://image.tmdb.org/t/p/original/7HR38hMBl23lf38MAN63y4pKsHz.jpg",
+            },
+        },
+    ];
     const [index, setIndex] = useState(0);
     const handleIndexIncrement = () => setIndex((i) => i + 1);
     const handleIndexDecrement = () => setIndex((i) => i - 1);
@@ -12,7 +31,7 @@ function Carousel() {
 
     return (
         <>
-            <div>
+            <div style={{ display: "flex" }}>
                 <button onClick={handleIndexDecrement}>LEFT</button>
                 <div style={{ fontSize: "4rem" }}>{images[index]}</div>
                 <button onClick={handleIndexIncrement}>RIGHT</button>
