5f6f4fca44f6ea4b3b014c24ef78c7bbe0ced36d
mastachiii <mastachii273@gmail.com>
2024-11-23 09:13:19 +0800

    FIX: Result of max page count calculation always needed to be rounded up

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -6,21 +6,26 @@ function Products({ items, currentPage, genreFilter, searchFilter, pageHandler }
     let itemsFiltered = items.filter((i) => i.title.includes(searchFilter));
     itemsFiltered = genreFilter ? itemsFiltered.filter((i) => i.genres.includes(genreFilter)) : itemsFiltered;
     const itemsPerPage = 20;
-    const maxPages = Math.round(itemsFiltered.length / itemsPerPage) - 1;
+    const maxPages = Math.ceil(itemsFiltered.length / itemsPerPage) - 1;
 
+    console.log({ maxPages });
     // Show items based on current page X the number of items you want to display on a page, second parameter keeps our itemsToShow inbounds.
     const itemsToShow = itemsFiltered.filter((a, b) => b >= currentPage * itemsPerPage && b < (currentPage + 1) * itemsPerPage);
 
     function handleNextPage() {
+        console.log({ currentPage, itemsToShow });
         if (currentPage < maxPages) pageHandler((p) => p + 1);
     }
 
     function handlePrevPage() {
-        pageHandler((p) => p - 1);
+        if (currentPage > 0) pageHandler((p) => p - 1);
     }
 
     return (
         <>
+            <p>
+                Page {currentPage + 1} out of {maxPages + 1}
+            </p>
             <div className={style.products}>
                 {itemsToShow && itemsToShow.map((a, b) => <ProductCard title={a.title} id={a.id} details={itemsToShow[b]} key={a.title} />)}
             </div>
