4480cd1a41d32b0ff41a20ec33cabe64038c9eed
mastachii <mastachii273@gmail.com>
2024-11-24 02:16:21 +0800

    FIX: Product runs into error when opened in a new tab

--- src/components/itemShop/itemShop.jsx
+++ src/components/itemShop/itemShop.jsx
@@ -1,50 +1,60 @@
 import PropTypes from "prop-types";
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, Link, useParams } from "react-router-dom";
 import { updateCart } from "../../scripts/shop";
 import ShopHeader from "../shopHeader/shopHeader";
 import styles from "./ItemShop.module.scss";
 import Footer from "../footer/footer";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { getProducts } from "../../scripts/shop";
 
 function ItemShop() {
-    const data = useLocation().state;
-    
-    return (
-        <>
-            <ShopHeader />
-            <main className={styles.itemContainer}>
-                <section className={styles.backdrop}>
-                    <img src={data.backdrops[0]} alt="backdrop" />
-                </section>
-                <section className={styles.details}>
-                    <div className={styles.poster}>
-                        <img src={data.poster} alt="" />
-                    </div>
-                    <div className={styles.text}>
-                        <h3>{data.title}</h3>
-                        <p>
-                            <em>Directed by: {data.directors.join(", ")}</em>
-                        </p>
-                        <p>
-                            <em>Starring: {data.actors.join(", ")}</em>{" "}
-                        </p>
-                        <p>{data.description}</p>
-                    </div>
-                    <div className={styles.pricing}>
-                        <p>${data.price}</p>
-                        <button
-                            onClick={() => {
-                                updateCart({ title: data.title, price: data.price }, "ADD");
-                            }}
-                        >
-                            Add To Cart
-                        </button>
-                    </div>
-                </section>
-            </main>
-            <Footer />
-        </>
-    );
+    const movieId = useParams().movieId;
+    const [data, setData] = useState(useLocation().state);
+
+    // If user opens product using a new tab, fetch product details else use state passed through Link.
+    if (!data) {
+        getProducts().then((r) => {
+            const newData = r.items.find((a) => a.id == movieId);
+            setData(newData);
+        });
+    } else {
+        return (
+            <>
+                <ShopHeader />
+                <main className={styles.itemContainer}>
+                    <section className={styles.backdrop}>
+                        <img src={data.backdrops[0]} alt="backdrop" />
+                    </section>
+                    <section className={styles.details}>
+                        <div className={styles.poster}>
+                            <img src={data.poster} alt="" />
+                        </div>
+                        <div className={styles.text}>
+                            <h3>{data.title}</h3>
+                            <p>
+                                <em>Directed by: {data.directors.join(", ")}</em>
+                            </p>
+                            <p>
+                                <em>Starring: {data.actors.join(", ")}</em>{" "}
+                            </p>
+                            <p>{data.description}</p>
+                        </div>
+                        <div className={styles.pricing}>
+                            <p>${data.price}</p>
+                            <button
+                                onClick={() => {
+                                    updateCart({ title: data.title, price: data.price }, "ADD");
+                                }}
+                            >
+                                Add To Cart
+                            </button>
+                        </div>
+                    </section>
+                </main>
+                <Footer />
+            </>
+        );
+    }
 }
 
 ItemShop.propTypes = {
