d9263972eaeffaeb11bdae3bca97a291236e5780
mastachiii <mastachii273@gmail.com>
2024-11-23 19:12:05 +0800

    FEAT: More Styling for Product Details

--- src/components/itemShop/itemShop.jsx
+++ src/components/itemShop/itemShop.jsx
@@ -2,34 +2,39 @@ import PropTypes from "prop-types";
 import { useLocation, Link } from "react-router-dom";
 import { updateCart } from "../../scripts/shop";
 import ShopHeader from "../shopHeader/shopHeader";
+import styles from "./ItemShop.module.scss";
 
 function ItemShop() {
     const data = useLocation().state;
     console.log(data);
     return (
-        <div>
+        <>
             <ShopHeader />
-            <div>
-                <img src={data.backdrops[0]} alt="backdrop" />
-            </div>
-            <section>
-                <div>POSTER</div>
-                <p>{data.title}</p>
-                <p>{data.directors.join(", ")}</p>
-                <p>{data.actors.join(", ")}</p>
-                <p>{data.description}</p>
-                <div>buying stuff</div>
-            </section>
-            <button
-                onClick={() => {
-                    updateCart({ title: data.title, price: data.price }, "ADD");
-                }}
-            >
-                Add To Cart
-            </button>
-            <Link to="/shop">GO BACK</Link>
-            <Link to="/shop/cart">GO TO CART</Link>
-        </div>
+            <main>
+                <section className={styles.backdrop}>
+                    <img src={data.backdrops[0]} alt="backdrop" />
+                </section>
+                <section className={styles.details}>
+                    <div className={styles.poster}>
+                        <img src={data.poster} alt="" />
+                    </div>
+                    <p>{data.title}</p>
+                    <p>{data.directors.join(", ")}</p>
+                    <p>{data.actors.join(", ")}</p>
+                    <p>{data.description}</p>
+                    <div>buying stuff</div>
+                </section>
+                <button
+                    onClick={() => {
+                        updateCart({ title: data.title, price: data.price }, "ADD");
+                    }}
+                >
+                    Add To Cart
+                </button>
+                <Link to="/shop">GO BACK</Link>
+                <Link to="/shop/cart">GO TO CART</Link>
+            </main>
+        </>
     );
 }
 
