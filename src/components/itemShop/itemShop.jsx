b3864e75fb412bbf7c8201e0fe2c6c3d799b9cd9
mastachiii <mastachii273@gmail.com>
2024-11-23 18:34:01 +0800

    FEAT: Added Styling For Detailed Product View

--- src/components/itemShop/itemShop.jsx
+++ src/components/itemShop/itemShop.jsx
@@ -1,13 +1,25 @@
 import PropTypes from "prop-types";
 import { useLocation, Link } from "react-router-dom";
 import { updateCart } from "../../scripts/shop";
+import ShopHeader from "../shopHeader/shopHeader";
 
 function ItemShop() {
     const data = useLocation().state;
-    
+    console.log(data);
     return (
         <div>
-            <p>{data.title}</p>
+            <ShopHeader />
+            <div>
+                <img src={data.backdrops[0]} alt="backdrop" />
+            </div>
+            <section>
+                <div>POSTER</div>
+                <p>{data.title}</p>
+                <p>{data.directors.join(", ")}</p>
+                <p>{data.actors.join(", ")}</p>
+                <p>{data.description}</p>
+                <div>buying stuff</div>
+            </section>
             <button
                 onClick={() => {
                     updateCart({ title: data.title, price: data.price }, "ADD");
